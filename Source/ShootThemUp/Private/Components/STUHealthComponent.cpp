ab8ee6cdf723e692d2b1f872c16b5eea09580a8f
Vasyl Bonkalo <vas.bon.design@gmail.com>
2022-03-14 20:40:19 +0200

    Apply Damage

--- Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
+++ Source/ShootThemUp/Private/Components/STUHealthComponent.cpp
@@ -3,6 +3,8 @@
 #include "Components/STUHealthComponent.h"
 #include "GameFramework/Actor.h"
 
+DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, All, All)
+
 USTUHealthComponent::USTUHealthComponent()
 {
     PrimaryComponentTick.bCanEverTick = false;
@@ -26,4 +28,5 @@ void USTUHealthComponent::OnTakeAnyDamage(
                                               class AController *InstigatedBy, AActor *DamageCauser)
 {
     Health -= Damage;
+    UE_LOG(LogHealthComponent, Display, TEXT("Damage: %f"), Damage);
 }
